d4b216a12968b2f6b149ccb89cfb3338728f872b
ALEX SANDER SILVA DE SOUZA <alex.sssouza7@senacsp.edu.br>
2020-03-04 21:42:06 -0300

    Inclusao de classe para objeto

---
+++ ADO1/src/Controller/Estado.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import Model.Estado;
+import DAO.Estado;
+
+/**
+ *
+ * @author alex.sssouza7
+ */
+public class Estado {
+    
+    public static boolean salvar(String pEstado, float pPib, String pRegiao){
+        
+    };
+}
